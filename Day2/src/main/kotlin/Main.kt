10c70c202e346921e7f85a0a8f5b664cb04a1906
Matt <matt@invevo.com>
2024-12-02 23:16:51 +0000

    Day 2 Part 2

--- Day2/src/main/kotlin/Main.kt
+++ Day2/src/main/kotlin/Main.kt
@@ -4,11 +4,17 @@ data class Report(val levels: List<Level>)
 data class Level(val value: Int)
 
 fun main() {
-    val part1Answer = input
+    val part1 = input
         .toReports()
         .count { it.isSafe() }
 
-    println("Part 1 Answer: $part1Answer")
+    println("Part 1 Answer: $part1")
+
+    val part2 = input
+        .toReports()
+        .count { it.isSafeWithTolerance() }
+
+    println("Part 2 Answer: $part2")
 }
 
 private fun List<String>.toReports(): List<Report> =
@@ -23,6 +29,17 @@ private fun List<String>.toReports(): List<Report> =
 private fun Report.isSafe(): Boolean =
     (this.levels.isAllIncreasing() || this.levels.isAllDecreasing()) &&
             this.levels.isAdjacentLevelsDifferenceWithinRange()
+
+private fun Report.isSafeWithTolerance(): Boolean =
+    (this.levels.isAllIncreasing() || this.levels.isAllDecreasing()) &&
+            this.levels.isAdjacentLevelsDifferenceWithinRange() ||
+    (0..< this.levels.size).any {index ->
+        val numberOfLevelsToRightOfIndex = this.levels.size - index - 1
+        val reducedLevels = listOf(this.levels.take(index), this.levels.takeLast(numberOfLevelsToRightOfIndex)).flatten() //this.levels.minus(this.levels[it])
+
+        (reducedLevels.isAllIncreasing() || reducedLevels.isAllDecreasing()) &&
+                reducedLevels.isAdjacentLevelsDifferenceWithinRange()
+    }
 
 private fun List<Level>.isAllIncreasing(): Boolean =
     this.mapIndexed { index, level ->
