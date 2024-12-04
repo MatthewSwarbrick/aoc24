6863c230debf5d032c04539041ee82257c2fe401
Matt <matt@invevo.com>
2024-12-04 21:01:21 +0000

    Day 4 Part 2

--- Day4/src/main/kotlin/main.kt
+++ Day4/src/main/kotlin/main.kt
@@ -2,11 +2,18 @@ data class Coord(val x: Int, val y: Int)
 
 fun main() {
     val part1 = input
-        .toWordGrid().apply { this.print() }
+        .toWordGrid()
         .toXmasWords()
         .count()
 
     println("Part 1: $part1")
+
+    val part2 = input
+        .toWordGrid().apply { this.print() }
+        .toMasXs()
+        .count()
+
+    println("Part 2: $part2")
 }
 
 private fun List<String>.toWordGrid(): Map<Coord, Char> =
@@ -24,6 +31,16 @@ private fun Map<Coord, Char>.toXmasWords(): Set<List<Coord>> =
         .flatten()
         .toSet()
 
+private fun Map<Coord, Char>.toMasXs(): Set<List<Coord>> =
+    this.entries
+        .asSequence()
+        .filter { it.value == 'M' }
+        .map { it.key }
+        .map { this.toMasWordsFromStartingCoord(it) }
+        .flatten()
+        .toSet()
+        .toMasXsFromDiagonalMas()
+
 private fun Map<Coord, Char>.toXmasWordsFromStartingCoord(coord: Coord): Set<List<Coord>> {
     val top = listOf(coord, coord.copy(y = coord.y - 1), coord.copy(y = coord.y - 2), coord.copy(y = coord.y - 3))
     val topRight = listOf(coord, coord.copy(x = coord.x + 1, y = coord.y - 1), coord.copy(x = coord.x + 2, y = coord.y - 2), coord.copy(x = coord.x + 3, y = coord.y - 3))
@@ -40,6 +57,35 @@ private fun Map<Coord, Char>.toXmasWordsFromStartingCoord(coord: Coord): Set<Lis
         .filter { this.getWordFromCoords(it) == "XMAS" }
         .toSet()
 }
+
+private fun Map<Coord, Char>.toMasWordsFromStartingCoord(coord: Coord): Set<List<Coord>> {
+    val topRight = listOf(coord, coord.copy(x = coord.x + 1, y = coord.y - 1), coord.copy(x = coord.x + 2, y = coord.y - 2))
+    val topLeft = listOf(coord, coord.copy(x = coord.x - 1, y = coord.y - 1), coord.copy(x = coord.x - 2, y = coord.y - 2))
+
+    val bottomRight = listOf(coord, coord.copy(x = coord.x + 1, y = coord.y + 1), coord.copy(x = coord.x + 2, y = coord.y + 2))
+    val bottomLeft = listOf(coord, coord.copy(x = coord.x - 1, y = coord.y + 1), coord.copy(x = coord.x - 2, y = coord.y + 2))
+
+    return listOf(topRight, topLeft, bottomRight, bottomLeft)
+        .filter { this.getWordFromCoords(it) == "MAS" }
+        .toSet()
+}
+
+private fun Set<List<Coord>>.toMasXsFromDiagonalMas(): Set<List<Coord>> =
+    this.mapNotNull { mas ->
+        val crossingMas = this.firstOrNull { (crossingM, crossingA, _) ->
+            val (m, a, _) = mas
+            crossingM != m && crossingA == a
+        }
+
+        if(crossingMas != null) {
+            listOf(mas, crossingMas)
+                .flatten()
+                .sortedBy { it.x }
+                .sortedBy { it.y }
+        } else {
+            null
+        }
+    }.toSet()
 
 private fun Map<Coord, Char>.getWordFromCoords(coords: List<Coord>): String =
     coords.joinToString("") {
