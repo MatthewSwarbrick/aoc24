bb1cd2322bc279e0211cde4e93dbae1865b3e7cc
Matt <matt@invevo.com>
2024-12-11 22:14:34 +0000

    Day 11 Part 2

--- Day11/src/main/kotlin/main.kt
+++ Day11/src/main/kotlin/main.kt
@@ -1,33 +1,44 @@
 data class Stone(val value: Long)
+data class StoneCacheKey(val stone: Stone, val iteration: Int)
 
 fun main() {
     val part1 = input
         .toStones()
-        .runBlinkSequence()
-        .count()
+        .sumOf {  it.runBlinkSequence() }
 
     println("Part 1: $part1")
+
+    val part2 = input
+        .toStones()
+        .sumOf { it.runBlinkSequence(iterations = 75) }
+
+    println("Part 2: $part2")
 }
 
-private fun List<Stone>.runBlinkSequence(iterations: Int = 25): List<Stone> =
-    (1..iterations).fold(this) { acc, _ -> acc.blink() }
+private fun Stone.runBlinkSequence(iterations: Int = 25): Long {
+    val cache = mutableMapOf<StoneCacheKey, Long>()
+    return this.runBlinkSequence(iterations, cache)
+}
+
+private fun Stone.runBlinkSequence(iterations: Int, cache: MutableMap<StoneCacheKey, Long>): Long {
+    val key = StoneCacheKey(this, iterations)
+    if (key in cache) return cache[key]!!
+
+    val result = when {
+        iterations == 0 -> 1
+        this.value == 0L -> Stone(1).runBlinkSequence(iterations - 1, cache)
+        "${this.value}".length % 2 == 0 -> {
+            val splitValue = "${this.value}".chunked("${this.value}".length / 2)
+                .map { Stone(it.toLong()) }
+            splitValue.sumOf { it.runBlinkSequence(iterations - 1, cache) }
+        }
+        else ->
+            Stone(this.value * 2024).runBlinkSequence(iterations - 1, cache)
+    }
+    cache[key] = result
+    return result
+}
 
 private fun String.toStones(): List<Stone> =
     this.split(" ").map { Stone(it.toLong()) }
 
-private fun List<Stone>.blink(): List<Stone> =
-    this.flatMap { it.blink() }
-
-private fun Stone.blink(): List<Stone> =
-    when {
-        this.value == 0L -> listOf(Stone(1))
-        "${this.value}".count() % 2 == 0 -> this.split()
-        else -> listOf(Stone(this.value * 2024))
-    }
-
-private fun Stone.split(): List<Stone> {
-    val originalStone = "${this.value}"
-    return originalStone
-        .chunked(originalStone.count() / 2)
-        .map { Stone(it.toLong()) }
-}
